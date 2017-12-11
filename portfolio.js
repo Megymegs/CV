840d8875d74e80d02699f1758a012d3aadf8931d
Megymegs <33827558+Megymegs@users.noreply.github.com>
2017-12-11 12:30:32 +0200

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,41 +1,29 @@
-$('#head_top_left').click(function() {
-  $('#head').fadeOut(2000).css('display', 'none');
-  $('#loading').fadeIn(1000).css('display', 'block');
-  $('#loading').delay(2000).fadeOut(2000);
+function loading (){
+  $('#head').fadeOut(700).css('display', 'none');
+  $('#loading').fadeIn(700).css('display', 'block');
+  $('#loading').delay(600).fadeOut(600);
   $('#mainBody').hide();
-  $('#mainBody').delay(3000).fadeIn(3000);
+  $('#mainBody').delay(1000).fadeIn(1000);
+}
+
+$('#head_top_left').click(function() {
+  loading()
 })
 
 $('#head_bottom_left').click(function() {
-  $('#head').fadeOut(2000).css('display', 'none');
-  $('#loading').fadeIn(1000).css('display', 'block');
-  $('#loading').delay(2000).fadeOut(2000);
-  $('#mainBody').hide();
-  $('#mainBody').delay(3000).fadeIn(3000);
+  loading()
 })
 
 $('#head_top_right').click(function() {
-  $('#head').fadeOut(2000).css('display', 'none');
-  $('#loading').fadeIn(1000).css('display', 'block');
-  $('#loading').delay(2000).fadeOut(2000);
-  $('#mainBody').hide();
-  $('#mainBody').delay(3000).fadeIn(3000);
+  loading()
 })
 
 $('#head_bottom_right').click(function() {
-  $('#head').fadeOut(2000).css('display', 'none');
-  $('#loading').fadeIn(1000).css('display', 'block');
-  $('#loading').delay(2000).fadeOut(2000);
-  $('#mainBody').hide();
-  $('#mainBody').delay(3000).fadeIn(3000);
+  loading()
 })
 
 $('#head_bottom').click(function() {
-  $('#head').fadeOut(2000).css('display', 'none');
-  $('#loading').fadeIn(1000).css('display', 'block');
-  $('#loading').delay(2000).fadeOut(2000);
-  $('#mainBody').hide();
-  $('#mainBody').delay(3000).fadeIn(3000);
+  loading()
 })
 
 
